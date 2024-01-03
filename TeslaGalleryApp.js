86ab64aed2283e307f057ca6877cf73e52a86485
Ornie <orniepay@buffalo.edu>
2024-01-03 15:38:48 -0500

    Learning CSS, JS, HTML & Improving Tesla Gallery App! Wohooo sophmore year CS winter break 2023-24 and Orn in the zone for that Tesla SWE intern job (yes I am sorry I know it is supposed to be a meaningful commit, not me having fun coding this project and talking to myself)

---
+++ TeslaGalleryApp.js
@@ -0,0 +1,17 @@
+//JS file
+
+//wait for entire HTML to be fully loaded and parsed
+document.addEventListener('DOMContentLoaded', function(){
+    var galleries = document.querySelectorAll('.js-flickity'); //selects all photos/elements that have class '.js-flickity'
+    galleries.forEach(function (gallery) { //loops through each of these galleries/elements
+        new Flickity(gallery, { //create a new Flickity interactive slideshow for each gallery element
+
+            //Interactive slideshow options :
+            cellAlign: 'left', //aligns images to left
+            contain: true, //make sure slideshow not blank at start or end
+            wrapAround: true, //allows slideshow to loop  back to beginning when it reaches end
+            autoPlay: 3000, //automatically changes slides every millisecond (3 seconds)
+            pauseAutoPlayOnHover: true //stop changing slides when mouse hovers over the slideshow
+        });
+    });
+});
